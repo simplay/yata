083e353877d30b421ca18aab78f6f2b7bcaa4707
simplay <silent.simplay@gmail.com>
2018-05-30 22:37:56 +0200

    Integrate history and router

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import rootStore from "./stores/root"
 import { Header, Container } from "semantic-ui-react"
 import "./App.css";
 import TodoList from "./components/TodoList";
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
 
 @observer
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.browserHistory = createBrowserHistory();
+        this.routingStore = new RouterStore();
+        this.history = syncHistoryWithStore(this.browserHistory, this.routingStore);
+    }
     render() {
         return (
             <Provider rootStore={rootStore}
-                todoStore={rootStore.todoStore}>
-                <div className="App">
-                    <Container>
-                        <Header as="h1">Todo App</Header>
-                        <TodoList />
-                    </Container>
-                </div>
+                      todoStore={rootStore.todoStore}
+                      routing={this.routingStore}>
+                <Router history={this.history}>
+                    <div className="App">
+                        <Route path="/todos" component={TodoList} />
+                    </div>
+                </Router>
             </Provider>
         );
     }
