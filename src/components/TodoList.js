5c2d723596e8c28f9be08b1ccd882838bfb7e21b
simplay <silent.simplay@gmail.com>
2018-06-05 21:05:47 +0200

    Change border color when element gets hovered

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -91,6 +91,10 @@ class TodoDetails extends Component {
 
 @inject("todoStore") @observer
 class TodoItem extends Component {
+    state = {
+        isHovered: false
+    };
+
     handleDeleteClick = (event) => {
         let activeTodo = this.props.todo;
         this.props.todoStore.deleteTodo(activeTodo);
@@ -101,20 +105,44 @@ class TodoItem extends Component {
         this.props.todoStore.setActiveTodo(this.props.todo);
     }
 
+    handleHoverOn = (event) => {
+        this.setState({
+            isHovered: true
+        });
+    }
+
+    handleHoverOff = (event) => {
+        this.setState({
+            isHovered: false
+        });
+    }
+
+    getTodoItemStyle = () => {
+        let todoStyle = {
+            marginBottom: "5px",
+            marginTop: "5px",
+            border: "3px solid"
+        };
+        todoStyle["color"] = this.state.isHovered ? "purple" : "black";
+        return todoStyle;
+    }
+
     render() {
         let todo = this.props.todo;
         return (
             <div>
             <TodoDetails todo={this.props.todo}/>
-              <Segment inverted
+            <Segment className="inverted"
                        size="large"
-                       style={{marginBottom: "5px", marginTop: "5px"}}
+                       style={this.getTodoItemStyle()}
+                       onMouseEnter={this.handleHoverOn}
+                       onMouseLeave={this.handleHoverOff}
                        onClick={this.handleSegmentClick}>
                   <List.Item>
                   <List.Content>
                       <Flex justify="space-between">
                           <Box>
-                              <List.Header>{todo.title}</List.Header>
+                              <List.Header style={{color: "white"}}>{todo.title}</List.Header>
                           </Box>
                           <Box>
                               <Button color="red" onClick={this.handleDeleteClick}> Delete </Button>
