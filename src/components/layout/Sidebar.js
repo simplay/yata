819d25ba2b68d46a07193e08e735eb079f0260f7
simplay <silent.simplay@gmail.com>
2018-07-13 15:41:16 +0200

    Introduce sidebar items

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import { Header, Button, Icon } from "semantic-ui-react"
+
+class SidebarItem extends React.Component {
+    render() {
+        return (
+            <div className="sidebar-item">
+                <Button
+                    color="black"
+                >
+                    <Icon name={this.props.iconName} />
+                    {this.props.label}
+                </Button>
+            </div>
+        )
+    }
+}
 
 class Sidebar extends React.Component {
     render() {
         return (
             <div id="sidebar">
-                Sidebar
+                <Header
+                    as='h3'
+                    inverted
+                >
+                    Filters
+                </Header>
+                <SidebarItem
+                    label="All"
+                    iconName="home"
+                />
+                <SidebarItem
+                    label="Incomplete"
+                    iconName="clipboard list"
+                />
+                <SidebarItem
+                    label="Done"
+                    iconName="clipboard check"
+                />
+                <Header
+                    as='h3'
+                    inverted
+                >
+                    Labels
+                </Header>
             </div>
         );
     }
